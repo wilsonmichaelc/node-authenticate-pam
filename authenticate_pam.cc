7e234bedf4ae9b9499593261b071efc759ca46b9
Michael Wilson <wilson.michael.c@gmail.com>
2016-03-18 12:30:11 -0400

    comment out pam_appl.h

--- authenticate_pam.cc
+++ authenticate_pam.cc
@@ -27,7 +27,7 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include <cstring>
 #include <stdlib.h>
 
-#include <security/pam_appl.h>
+//#include <security/pam_appl.h>
 
 using namespace v8;
 
